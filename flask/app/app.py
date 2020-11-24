aafd0c430e132e6ad04412f0e175086af8f4a0e7
Jerrard Lawrence <jerrard96@gmail.com>
2020-11-24 10:53:38 +0000

    flask app.py working with main binary route

--- flask/app/app.py
+++ flask/app/app.py
@@ -5,6 +5,7 @@ import json
 
 # Init App
 app = Flask(__name__)
+app.secret_key = 'S3cR3t'
 # Upload folder destination & set allowed extensions
 upload_folder = './uploaded'
 allowed_extensions = None
@@ -18,45 +19,37 @@ def allowed_file(filename):
 #Function to format the response
 def format_response(file):
     headers = {'Accept': 'application/json'}
+    nlp_headers = {'Content-Type': 'application/json'}
     resp_meta = requests.put(meta_endpoint, data=file, headers=headers)
     meta = resp_meta.json()
     resp_extraction = requests.put(ocr_endpoint, data=file)
     extracted = resp_extraction.text
     extraction = extracted.replace('\n', ' ')
+    
+    if extraction:
+        nlp_data = {'text': extraction, 'model': 'en'}
+        nlp_response = requests.post(nlp_endoint, data=json.dumps(nlp_data), headers=nlp_headers)
+        nlp_extraction = nlp_response.json()
+    else:
+        nlp_extraction = "No data."
     # Create JSON structure
     formatted_response = {
         "meta": meta,
-        "extractions": extraction
+        "extractions": [
+            {
+                "ocr_extraction": extraction,
+                "nlp_extraction": nlp_extraction
+            }
+        ]
     }
     #Format JSON
     formatted_response_json = json.dumps(formatted_response, indent=4)
 
     return formatted_response_json
 
-
 # Main Route:
-# Use GET method to make request via web browser UI
-# Use POST method with form-data and file to upload, named 'file' via Postman
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user doesn't select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            return format_response(file)
-    return render_template('index.html')
-
-# Secondary Route:
 # Use POST method with binary and file to upload via Postman
-@app.route('/binary', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_binary_file():
     file_as_binary = request.get_data()
     return format_response(file_as_binary)
@@ -68,6 +61,27 @@ if __name__ == "__main__":
         allowed_extensions = config["allowed_extensions"]
         meta_endpoint = config["meta_endpoint"]
         ocr_endpoint = config["ocr_endpoint"]
+        nlp_endoint = config["nlp_endpoint"]
         host_zero = config["host_zero"]
+
+# Optional Route?:
+# Use GET method to make request via web browser UI
+# Use POST method with form-data and file to upload, named 'file' via Postman
+# @app.route('/', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user doesn't select file, browser also
+#         # submit an empty part without filename
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             return format_response(file)
+#     return render_template('index.html')
 
     app.run(host=host_zero, debug=True)
